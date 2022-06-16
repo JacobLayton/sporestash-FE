0589b82f4dd25925ec3274ca74732e46d379265a
Jacob Layton <jlayjlay3@gmail.com>
2022-06-15 19:00:51 -0700

    Add new logo to Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import sporeStashLogo from "../img/Final_deer_flat.png";
-import "../styles/landing.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import sporeStashLogo from '../img/Final_deer_flat.png';
+import sporeStashLogo2 from '../img/Mustacheflatfinal1.png';
+import '../styles/landing.css';
 
 function Landing() {
-  return (
-    <div className="landing-container">
-      <h1>Spore Stash</h1>
-      <img src={sporeStashLogo} alt="Spore Stash Logo" />
-      <Link to="/shop">
-        <button className="landing-button">SHOP</button>
-      </Link>
-    </div>
-  );
+	return (
+		<div className='landing-container'>
+			<h1>Spore Stash</h1>
+			<img src={sporeStashLogo2} alt='Spore Stash Logo' />
+			<Link to='/shop'>
+				<button className='landing-button'>SHOP</button>
+			</Link>
+		</div>
+	);
 }
 
 export default Landing;
